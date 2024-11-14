5eb25d66bb1333c805a784b21284c4834503b627
Lucas <127881339+MirandaWopps@users.noreply.github.com>
2024-11-13 22:19:50 -0300

    Update testa.c

--- testa.c
+++ testa.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include "cria_func.h"
 
-int main (void){
-//  cria_func();
-  
-  
-  return 0;
+int main(void) {
+	// Primeiro parametro: endereco
+	// colocAq
+	// Segundo: descricao dos parametros
+
+	// Terceiro: qtd parametros
+
+	// Quarto: vetor com o codigos
+
+
+	//cria_func(void* f, DescParam params[], int n, unsigned char codigo[]);
+	unsigned char codigo[32];
+	printf("%d, size of void %d \n", sizeof(codigo), sizeof(int) );
+
+	return 0;
 }
