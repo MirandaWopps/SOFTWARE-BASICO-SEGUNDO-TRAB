53047ae072464615a0681cdbd29707dce741df01
MirandaWopps <lucas.pereiraskeit@gmail.com>
2024-11-15 14:18:43 -0300

    after breaking out doubts in class, we go !

--- cria_func.c
+++ cria_func.c
@@ -1,67 +1,18 @@
-#include "cria_func.h"
+/* Lucas Ebrenz Fernandes Pereira 20011488 3WC */
+/* version 0.1 */
+
+//#define DEBUG // comente essa linha para desativar o modo debug.
 #include <stdio.h>
-/** Cria_func
+#include "cria_func.h"
+/**
+* Cria funcoes.
 * 
-*
-* @authors Eu
-* @param f tem endereço da func original
-* @param params descricao dos params para chamar essa func. Variam entre PARAM(o parametro nao amarrado, ou seja, deve ser recebido pela nova func e repassado a func orgin), FIX(o parametro deve ser amarrado a um valor constante fornecido no campo valor) e IND(o parametro deve ser amarrado a uma variavel cujo endereco esta no campo valor, ou seja, passado a func original o valor corrente dessa var.
-* @param n numero de parametros descritos por params e codigo
-* @param codigo vetor onde code gerado sera gravado
-* @return nothing.
+* @param f  Tem o endereco da funcao original a ser chamada pelo cod gerado.
+* @param params[] array contendo desc dos params para chamar essa func: max 3
+* @param n Qnts params estao no array params[].
+* @param codigo[] Vetor onde devera ser gravado o codigo gerado.
+* @return Essa funcao nao retorna nada.
 */
-void cria_func(void* f, DescParam params[], int n, unsigned char codigo[]) {
-	// Aloque espaco para funcao na memoria
-	unsigned char* funcao;
-	funcao = &f; // aponta para o endereco desejado, assim, construimos o codigo la.
-	funcao = (unsigned char*)malloc(sizeof(codigo) );// lembre de alocar 
-
-	// preciso ler o vetor todo em "codigo" e por na funcao "funcao"
-	//for (int i = 0; i < n; i++) { /*estamos varrendo o vetor fazendo alteracoes dos registers por seus valores*/
-	for (int j = 0;   j < sizeof(codigo);     j++) {
-		/*if (codigo[j] == 1) {//rdi
-			funcao[j] = ;
-		}
-		else if (codigo[j] == 2) {//rsi
-			funcao[j] = ;
-		}
-		else if (codigo[j] == 3) {// rdx
-			funcao[j] = ;
-		}
-		else if (codigo[j] == 4) {// edi
-			funcao[j] = ;
-		}
-		else if (codigo[j] == 5) {//esi
-			funcao[j] = ;
-		}
-		else if (codigo[j] == 6) { // edx
-			funcao[j] = ;
-		}
-		else {
-			funcao[j] = codigo[j];
-			printf("%c ", codigo[j]);// so uma linha de debug
-		}*/
-		funcao[j] = codigo[j];
-	}
-	//}
-	//
-
-	//
-
-
-
-	//   preciso substituir os valoers que equivalem a %rdi, %rsi, %rdx  e %edi, %esi e %edx. Eles variam
-	// if sizeof(valor) == sizeof(int) ou ponteiro.
-	//     anexa os valores dos parametros aos seus respectivos registradores de 4 bytes 	
-	// else /*sabemos que e 8 bytes*/
-	//		anexa valores dos parametros de 8 bytes em seus respectivos codigos(identificadores)
-	//
-	//
-	//
-	//
-	for (unsigned char *p=f; p != NULL; p++) { // linha para debug
-		int i = 0;
-		printf("(%c) " , p);
-		
-	}
+void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
+    
 }
--- testa.c
+++ testa.c
@@ -2,18 +2,6 @@
 #include "cria_func.h"
 
 int main(void) {
-	// Primeiro parametro: endereco
-	// colocAq
-	// Segundo: descricao dos parametros
-
-	// Terceiro: qtd parametros
-
-	// Quarto: vetor com o codigos
-
-
-	//cria_func(void* f, DescParam params[], int n, unsigned char codigo[]);
-	unsigned char codigo[32];
-	printf("%d, size of void %d \n", sizeof(codigo), sizeof(int) );
 
 	return 0;
 }
