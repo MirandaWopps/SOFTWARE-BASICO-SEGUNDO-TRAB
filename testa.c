74a3a33077467a7620e4b8b1d545a7fe97ab82b4
Lucas <127881339+MirandaWopps@users.noreply.github.com>
2024-11-17 17:24:40 -0300

    Update testa.c

--- testa.c
+++ testa.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "cria_func.h"
 
+char fixa[] = "asd";
+
 typedef int (*func_ptr)(int x);
 
 int mult(int x, int y) {
@@ -15,27 +17,33 @@ void dump (void *p, int n) {
   }
 }
 
-int main(void) {
+int main (void) {
   DescParam params[2];
   func_ptr f_mult;
   int i;
   unsigned char codigo[500];
-  
-// caracterizar parametros
 
   params[0].tipo_val = INT_PAR; /* o primeiro parãmetro de mult é int */
-  params[0].orig_val = PARAM;   /* a nova função repassa seu parämetro */
+  params[0].orig_val = FIX;   /* a nova função repassa seu parämetro */
+  params[0].valor.v_int = 10;
 
-  params[1].tipo_val = INT_PAR; /* o segundo parâmetro de mult é int */
-  params[1].orig_val = FIX;     /* a nova função passa para mult a constante 10 */
+
+/*
+  params[1].tipo_val = INT_PAR; /* o segundo parâmetro de mult é int *
+  params[1].orig_val = FIX;     /* a nova função passa para mult a constante 10 *
   params[1].valor.v_int = 10;
-    
-//void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){    
-    cria_func(mult, params, 2 , codigo);
-    for (int i=0; i<50 ;i++){
-      printf("%x ", codigo[i] );
-    }    	
-    dump(codigo,50);
-    
-	return 0;
+*/
+  cria_func (mult, params, 2, codigo);
+  f_mult = (func_ptr) codigo;   
+
+  for (int j=0; j<50; j++){
+    printf("%x ", codigo[j]);
+  }
+
+/*
+  for (i = 1; i <=10; i++) {
+    printf("%d\n", f_mult(i)); /* a nova função só recebe um argumento 
+  }*/
+
+  return 0;
 }
