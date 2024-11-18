b4170bd67b0a13eabd8327543942cd0bdaf7fde5
Lucas <127881339+MirandaWopps@users.noreply.github.com>
2024-11-18 18:36:45 -0300

    Update cria_func.c

--- cria_func.c
+++ cria_func.c
@@ -30,7 +30,7 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
     }*/
     /*ARRUMAR 1st PARAMETRO*/
   if (n!=0){ // se existe algum parametro
-    if (params[0].orig_val = PARAM  )   {
+    if (params[0].orig_val == PARAM  )   {
       if (params[0].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
         codigo[8] = 0x89; // movl %edi,%edi 
         codigo[9] = 0xff; // 
@@ -41,8 +41,12 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
         codigo[10]= 0xff;
         i+=3;
       }
+        #ifdef DEBUG
+        printf("entrou (first param)PARAM\n");
+        #endif
       
-    } else if (params[0].orig_val = FIX){// VALOR FIXO A SER PASSADO !
+      
+    } else if (params[0].orig_val == FIX){// VALOR FIXO A SER PASSADO !
         if (params[0].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
           codigo[8] = 0xbf; // movl $0x5,%edi
           codigo[9] = (unsigned char)params[0].valor.v_int; // 2
@@ -61,19 +65,11 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
           i+=7;
         }
         #ifdef DEBUG
-        printf("entrou aq\n");
+        printf("entrou (first param)FIX\n");
         #endif
-        
     }
     else { // IND- endereco
-          if (params[0].tipo_val == INT_PAR){ // int: 32bits, logo, eax
-          codigo[8] = 0xbf; // movl $0x5,%edi
-          codigo[9] = (unsigned char)params[0].valor.v_int; // 2
-          codigo[10]= 0x00; // 3
-          codigo[11]= 0x00; // 4
-          codigo[12]= 0x00; //5
-          i+=5;
-        }else{ // PTR_ PAR pede 64 bits
+          // PTR_ PAR pede 64 bits
           codigo[8] = 0x48; // movq $0x5,%rdi
           codigo[9] = 0xc7; // 2
           codigo[10]= 0xc7; // 3
@@ -82,50 +78,140 @@ void cria_func (void* f, DescParam params[], int n, unsigned char codigo[]){
           codigo[13]= 0x00; // 6
           codigo[14]= 0x00; // 7
           i+=7;
-        }
+        
+        #ifdef DEBUG
+        printf("entrou (first param)IND\n");
+        #endif
     }// fim ind
   }//fim first param
    
    
-   /*
-  /*ARRUMAR 2st PARAMETRO*
-  if (n<2){ // se existe algum parametro 
-    if (params[2].orig_val = PARAM  )   { // CASO REPASSSE O PARAM ('PARAM')
-       if (params[2].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
-          codigo[8] = 0x89; // movl %esi,%esi 
-          codigo[9] = 0xf6; // 
-          i+=2;
-       } else{ // PTR_ PAR pede 64 bits
-          codigo[8] = 0x48; // movq %rsi, %rsi
-          codigo[9] = 0x89;
-          codigo[10]= 0xf6;
-          i+=3;
-      }
-      
-    }
-  }  
-    
-    
-    /*ARRUMAR 3st PARAMETRO
+  
+  
+  
+  
+  
+  //ARRUMAR 2st PARAMETRO
   if (n<3){ // se existe algum parametro 
-    if (params[2].orig_val = PARAM  )   { // CASO REPASSSE O PARAM ('PARAM')
-       if (params[2].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
-          codigo[8] = 0x89; // movl %edx,%edx 
-          codigo[9] = 0xd2; // 
-          i+=2;
-       } else{ // PTR_ PAR pede 64 bits
-          codigo[8] = 0x48; // movq %rdx, %rdx
-          codigo[9] = 0x89; 
-          codigo[10]= 0xd2;
-          i+=3;
-      }
-      
-    } else { // 
-        
-        
-    }
+  #ifdef DEBUG
+  printf("CAPTOUU EXISTENCIA DE 2 PARAMETROS\n");
+  #endif
+      if (params[1].orig_val == PARAM ){ // CASO REPASSSE O PARAM ('PARAM')
+          if (params[1].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
+              i++; codigo[i]= 0x89; // movl %esi,%esi 
+              i++; codigo[i]= 0xf6; // 
+           } else{ // PTR_ PAR pede 64 bits
+              i++; codigo[i]= 0x48; // movq %rsi, %rsi
+              i++; codigo[i]= 0x89;
+              i++; codigo[i]= 0xf6;
+           }
+      #ifdef DEBUG
+      printf("(SECOND param)PARAM\n");
+      #endif
+    }// fim PARAM 
     
-  }  */
+    else if (params[1].orig_val == FIX){// VALOR FIXO A SER PASSADO !
+        if (params[1].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
+          i++; codigo[i] = 0xbe; // movl $0x5,%edi
+          i++; codigo[i] = (unsigned char)params[1].valor.v_int; // 2
+          i++; codigo[i] = 0x00; // 3
+          i++; codigo[i] = 0x00; // 4
+          i++; codigo[i] = 0x00; //5
+        }else{ // PTR_ PAR pede 64 bits
+          i++; codigo[i] = 0x48; // movq $0x5,%rdi
+          i++; codigo[i] = 0xc7; // 2
+          i++; codigo[i]= 0xc6; // 3
+          i++; codigo[i] = (unsigned char)params[1].valor.v_ptr;
+          i++; codigo[i]= 0x00; // 5
+          i++; codigo[i] = 0x00; // 6
+          i++; codigo[i]= 0x00; // 7
+        }
+        #ifdef DEBUG
+        printf("(SECOND param)FIX\n");
+        #endif
+    }// fim FIX
+    
+    else { // IND- endereco
+        
+   // PTR_ PAR pede 64 bits - estamos excluindo o outro caso
+          i++; codigo[i] = 0x48; // movq $0x5,%rdi
+          i++; codigo[i] = 0xc7; // 2
+          i++; codigo[i]= 0xc6; // 3
+          i++; codigo[i]= (unsigned char)params[1].valor.v_ptr;// e uma union
+          i++; codigo[i]= 0x00; // 5
+          i++; codigo[i]= 0x00; // 6
+          i++; codigo[i]= 0x00; // 7
+        
+        #ifdef DEBUG
+        printf("(SECOND param)IND\n");
+        #endif
+    }// fim ind
+  }//fim segundo param
+  
+  
+  
+  
+  
+  
+    
+    
+        //ARRUMAR 3st PARAMETRO
+  if (n<4){ // se existe algum parametro 
+    #ifdef DEBUG
+    printf("CAPTOUU EXISTENCIA DE 2 PARAMETROS\n");
+    #endif
+      if (params[2].orig_val == PARAM ){ // CASO REPASSSE O PARAM ('PARAM')
+          if (params[1].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
+              i++; codigo[i]= 0x89; // movl %esi,%esi 
+              i++; codigo[i]= 0xd2; // 
+           } else{ // PTR_ PAR pede 64 bits
+              i++; codigo[i]= 0x48; // movq %rsi, %rsi
+              i++; codigo[i]= 0x89;
+              i++; codigo[i]= 0xd2;
+           }
+      #ifdef DEBUG
+      printf("(SECOND param)PARAM\n");
+      #endif
+    }// fim PARAM 
+    
+    else if (params[2].orig_val == FIX){// VALOR FIXO A SER PASSADO !
+        if (params[1].tipo_val == INT_PAR){ // int param: reg de 32, logo, eax
+          i++; codigo[i] = 0xba; // movl $0x5,%edi
+          i++; codigo[i] = (unsigned char)params[1].valor.v_int; // 2
+          i++; codigo[i] = 0x00; // 3
+          i++; codigo[i] = 0x00; // 4
+          i++; codigo[i] = 0x00; //5
+        }else{ // PTR_ PAR pede 64 bits
+          i++; codigo[i] = 0x48; // movq $0x5,%rdi
+          i++; codigo[i] = 0xc7; // 2
+          i++; codigo[i]= 0xc2; // 3
+          i++; codigo[i] = (unsigned char)params[1].valor.v_ptr;
+          i++; codigo[i]= 0x00; // 5
+          i++; codigo[i] = 0x00; // 6
+          i++; codigo[i]= 0x00; // 7
+        }
+        #ifdef DEBUG
+        printf("(SECOND param)FIX\n");
+        #endif
+    }// fim FIX
+    
+    else { // IND- endereco
+        
+   // PTR_ PAR pede 64 bits - estamos excluindo o outro caso
+          i++; codigo[i]= 0x48; // movq $0x5,%rdi
+          i++; codigo[i]= 0xc7; // 2
+          i++; codigo[i]= 0xc2; // 3
+          i++; codigo[i]= (unsigned char)params[1].valor.v_ptr;// e uma union
+          i++; codigo[i]= 0x00; // 5
+          i++; codigo[i]= 0x00; // 6
+          i++; codigo[i]= 0x00; // 7
+        
+        #ifdef DEBUG
+        printf("(SECOND param)IND\n");
+        #endif
+    }// fim ind
+    
+  }// fim terceiro parametro  
     
     
     /* CALL F */
